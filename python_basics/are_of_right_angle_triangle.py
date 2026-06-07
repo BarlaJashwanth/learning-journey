c9da295413eb47a15e1782ed7316a100152c0340
Barla Jashwanth <jashwanthbarla@gmail.com>
2026-06-07 08:20:37 +0530

    area_of_right_angle_triangle.py

---
+++ python_basics/are_of_right_angle_triangle.py
@@ -0,0 +1,9 @@
+"""
+let Right angle triangle have sides
+height H and base B
+area = 0.5 * B * H
+"""
+H = float(input("Enter height: "))
+B = float(input("Enter base  : "))
+area = 0.5 * B * H
+print("Area of the triangle is ",round(area ,2))
