08f7c33d0a316fed76b369019c92b7e47c001377
Barla Jashwanth <jashwanthbarla@gmail.com>
2026-06-07 07:33:06 +0530

    update to area_of_triangle.py

--- area_of_triangle.py
+++ area_of_triangle.py
@@ -1,5 +1,5 @@
 """
-when all the length of the sides of the triangle is known - a, b, c
+where all the length of the sides of the triangle is known - a, b, c
 semi perimeter (s) = (a + b + c ) / 2
 Area = square root of (s * (s - a) * (s - b) * (s - c))
 """
